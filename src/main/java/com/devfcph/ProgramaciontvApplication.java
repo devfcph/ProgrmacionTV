23a3fed96b5aca91ed08c5851d61d78cecc26fd9
Francisco Pérez <devfcph@gmail.com>
2024-04-24 00:27:00 -0600

    First commit

---
+++ src/main/java/com/devfcph/ProgramaciontvApplication.java
@@ -0,0 +1,13 @@
+package com.devfcph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaciontvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramaciontvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devfcph/ProgramaciontvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfcph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramaciontvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
